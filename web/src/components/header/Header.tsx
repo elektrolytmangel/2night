a52cbbdfbedcfd779b3125f0aa9c5b2e9b511477
Marc Muehlheim <marc.muehlheim@hftm.ch>
2023-10-31 07:51:30 +0100

    Format date to eu standard #2

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -4,19 +4,21 @@ import {
   getSundayFromWeekDay,
 } from "../../services/date.service";
 import "./Header.css";
+import { DateTime } from "luxon";
 
 type Props = {
   dayInWeek: Date;
 };
 
 export const Header = (props: Props) => {
-  const monday = getMondaFromWeekDay(props.dayInWeek);
-  const sunday = getSundayFromWeekDay(props.dayInWeek);
+  const monday = DateTime.fromJSDate(getMondaFromWeekDay(props.dayInWeek));
+  const sunday = DateTime.fromJSDate(getSundayFromWeekDay(props.dayInWeek));
 
   return (
     <div className="w-100 d-flex gap-1 mb-3 d-flex align-items-center justify-content-between">
       <p className="basic-text">
-        {monday.toLocaleDateString()} - {sunday.toLocaleDateString()}
+        {monday.toFormat('dd.MM.yyyy')} -{" "}
+        {sunday.toFormat('dd.MM.yyyy')}
       </p>
       <a href="https://www.instagram.com/2night.ch/" target="__blank">
         <FaInstagram color="#eb539f" size={"2rem"} />
